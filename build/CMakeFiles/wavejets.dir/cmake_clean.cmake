555c955728b91d2ff9b7ce746e28fa51d9980e5c
mportefaix <maxime.portefaix@gmail.com>
2021-04-26 16:50:18 +0200

    Project folder, vscode project, cmakelists, main

---
+++ build/CMakeFiles/wavejets.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wavejets.dir/src/main.cpp.o"
+  "wavejets"
+  "wavejets.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wavejets.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
