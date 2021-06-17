7b5113427c0cf2c9779e15d68489339948d43429
mportefaix <maxime.portefaix@gmail.com>
2021-06-17 16:12:34 +0200

    better slider app + start for bunny

---
+++ tests/src/Bunny/main.cpp
@@ -0,0 +1,71 @@
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include <opencv2/viz/viz3d.hpp>
+
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <vector>
+
+
+
+using namespace cv;
+using namespace viz;
+using namespace std;
+
+
+namespace fs = std::filesystem;
+
+
+
+Viz3d init_window()
+{
+    Viz3d cam { "Wavejets Demo" };
+    cam.setRepresentation(REPRESENTATION_SURFACE);
+    cam.setBackgroundColor(Color::white());
+    //cam.showWidget("coordinate", WCoordinateSystem(10));
+    return cam;
+}
+
+
+int main(int argc, char** argv)
+{
+    if (argc != 1)
+    {
+        cout << "wrong args : " << argc << argv << endl;
+    }
+    auto cam = init_window();
+
+    ifstream bunny_txt;
+    string path = fs::current_path().string() + "/../../resources/bunny_dots.txt";
+    bunny_txt.open(path);
+    if (!bunny_txt.is_open())
+    {
+        cerr << "Cannot open bunny.txt with path : " << path << endl;
+        return 1;
+    }
+    string line;
+    vector<Point3d> pts;
+
+    while(getline(bunny_txt, line))
+    {
+        double x = stod(line.substr(0, line.find(' ')));
+        line = line.substr(line.find(' ') + 1);
+        double y = stod(line.substr(0, line.find(' ')));
+        line = line.substr(line.find(' ') + 1);
+        double z = stod(line.substr(0, line.find(' ')));
+        pts.emplace_back(Point3d { x, y, z });
+    }
+    cam.showWidget("rabbit", WCloud(pts, Color::black()));
+    /*for (u_int i = 0; i <= pts.size() - 3; ++i)
+    {
+        vector<Point3d> triangle { pts.at(i), pts.at(i+1), pts.at(i+2) };
+        Mat polygon3 = (Mat_<int>(1,4) << 3, 0, 1, 2);
+        string name = "tri" + to_string(i);
+        cam.showWidget(name, WMesh { triangle, polygon3 });
+    }*/
+
+    cam.spin();
+
+    return 0;
+}
--- tests/src/SlidersWavejet/main.cpp
+++ tests/src/SlidersWavejet/main.cpp
@@ -1,4 +1,5 @@
 #include "opencv2/imgcodecs.hpp"
+#include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
 #include <opencv2/viz/viz3d.hpp>
 
@@ -7,7 +8,8 @@
 #include <thread>
 #include <chrono>
 #include <mutex>
-
+#include <string>
+#include <filesystem>
 
 
 #include "phi.hpp"
@@ -18,23 +20,22 @@ using namespace cv;
 using namespace viz;
 using namespace std;
 
+namespace fs = std::filesystem;
+
+
 
 Viz3d init_window();
 void vizcam();
 
+u_int k = 4;
 
 const double scale_0 = 2;
 const double scale_1 = 10;
 const double scale_2 = 30;
 const double scale_3 = 80;
+const double scale_4 = 150;
 
-const int max_0_0 = 10;
-const int max_1_1 = 10;
-const int max_2_2 = 10;
-const int max_2_0 = 10;
-const int max_3_3 = 10;
-const int max_3_1 = 10;
-
+const int max_scale = 10;
 
 int real_0_0 = 0; int imag_0_0 = 0;
 int real_1_1 = 0; int imag_1_1 = 0;
@@ -42,6 +43,10 @@ int real_2_2 = 0; int imag_2_2 = 0;
 int real_2_0 = 0; int imag_2_0 = 0;
 int real_3_3 = 0; int imag_3_3 = 0;
 int real_3_1 = 0; int imag_3_1 = 0;
+int real_4_4 = 0; int imag_4_4 = 0;
+int real_4_2 = 0; int imag_4_2 = 0;
+int real_4_0 = 0; int imag_4_0 = 0;
+
 
 Viz3d cam;
 
@@ -50,6 +55,7 @@ std::mutex lock_track;
 Viz3d init_window()
 {
     Viz3d cam { "Wavejets Demo" };
+    cam.setRepresentation(REPRESENTATION_SURFACE);
     cam.setBackgroundColor(Color::white());
     cam.showWidget("coordinate", WCoordinateSystem(10));
     return cam;
@@ -64,10 +70,48 @@ void vizcam()
     cam.spin();
 }
 
+static Mat wavejet_mat (int k, int n)
+{
+    Mat img;
+    string file_name  = to_string(k) + "_" + to_string(n) + ".png";
+    string image_path = samples::findFile(fs::current_path().string() + "/output/" + to_string(k) + "-Wavejet/" + file_name, false, true);
+    if (image_path == "")
+    {
+        cerr << "Cannot found file" << file_name << ", please compute corresponding KWavejet before" << endl;
+        return img;
+    }
+    img = imread(image_path, IMREAD_COLOR);
+    if (img.empty())
+    {
+        cerr << "Error occur while opening image" << endl;
+        return img;
+    }
+    resize(img, img, Size(), 0.1, 0.1);
+    rotate(img, img, ROTATE_90_CLOCKWISE);
+    return img;
+}
+
+static void display_real_wavejets()
+{
+    Mat wj_add;
+    if (real_0_0 != 0) wj_add.push_back(wavejet_mat(0, 0));
+    if (real_1_1 != 0) wj_add.push_back(wavejet_mat(1, 1));
+    if (real_2_2 != 0) wj_add.push_back(wavejet_mat(2, 2));
+    if (real_2_0 != 0) wj_add.push_back(wavejet_mat(2, 0));
+    if (real_3_3 != 0) wj_add.push_back(wavejet_mat(3, 3));
+    if (real_3_1 != 0) wj_add.push_back(wavejet_mat(3, 1));
+    if (real_4_4 != 0) wj_add.push_back(wavejet_mat(4, 4));
+    if (real_4_2 != 0) wj_add.push_back(wavejet_mat(4, 2));
+    if (real_4_0 != 0) wj_add.push_back(wavejet_mat(4, 0));
+    if (!wj_add.empty())
+    {
+        rotate(wj_add, wj_add, ROTATE_90_CLOCKWISE);
+        imshow("Tracks", wj_add);
+    }
+}
+
 static void on_trackbar( int, void* )
 {
-    u_int k = 3;
-
     Phi phi { k };
     phi.of_zeros();
     phi.wiseset(0, 0, complex<double> { real_0_0 / scale_0, imag_0_0 / scale_0 });
@@ -76,7 +120,12 @@ static void on_trackbar( int, void* )
     phi.wiseset(2, 0, complex<double> { real_2_0 / scale_2, imag_2_0 / scale_2 });
     phi.wiseset(3, 3, complex<double> { real_3_3 / scale_3, imag_3_3 / scale_3 });
     phi.wiseset(3, 1, complex<double> { real_3_1 / scale_3, imag_3_1 / scale_3 });
+    phi.wiseset(4, 4, complex<double> { real_4_4 / scale_4, imag_4_4 / scale_4 });
+    phi.wiseset(4, 2, complex<double> { real_4_2 / scale_4, imag_4_2 / scale_4 });
+    phi.wiseset(4, 0, complex<double> { real_4_0 / scale_4, imag_4_0 / scale_4 });
     phi.prompt_display();
+
+    display_real_wavejets();
 
     WavejetDisplay wd { phi, k };
     wd.compute_and_find_point_position();
@@ -97,24 +146,34 @@ int main(int argc, char** argv)
     thread demo_thread { vizcam };
 
     namedWindow("Tracks", WINDOW_AUTOSIZE); // Create Window
+    resizeWindow("Tracks", Size { 1000, 500});
     
-    createTrackbar( "Phi(0,0) (real)", "Tracks", &real_0_0, max_0_0, on_trackbar );
-    createTrackbar( "Phi(0,0) (imag)", "Tracks", &imag_0_0, max_0_0, on_trackbar );
+    createTrackbar( "Phi(0,0) (real)", "Tracks", &real_0_0, max_scale, on_trackbar );
+    createTrackbar( "Phi(0,0) (imag)", "Tracks", &imag_0_0, max_scale, on_trackbar );
 
-    createTrackbar( "Phi(1,1) (real)", "Tracks", &real_1_1, max_1_1, on_trackbar );
-    createTrackbar( "Phi(1,1) (imag)", "Tracks", &imag_1_1, max_1_1, on_trackbar );
+    createTrackbar( "Phi(1,1) (real)", "Tracks", &real_1_1, max_scale, on_trackbar );
+    createTrackbar( "Phi(1,1) (imag)", "Tracks", &imag_1_1, max_scale, on_trackbar );
 
-    createTrackbar( "Phi(2,2) (real)", "Tracks", &real_2_2, max_2_2, on_trackbar );
-    createTrackbar( "Phi(2,2) (imag)", "Tracks", &imag_2_2, max_2_2, on_trackbar );
+    createTrackbar( "Phi(2,2) (real)", "Tracks", &real_2_2, max_scale, on_trackbar );
+    createTrackbar( "Phi(2,2) (imag)", "Tracks", &imag_2_2, max_scale, on_trackbar );
 
-    createTrackbar( "Phi(2,0) (real)", "Tracks", &real_2_0, max_2_0, on_trackbar );
-    createTrackbar( "Phi(2,0) (imag)", "Tracks", &imag_2_0, max_2_0, on_trackbar );
+    createTrackbar( "Phi(2,0) (real)", "Tracks", &real_2_0, max_scale, on_trackbar );
+    createTrackbar( "Phi(2,0) (imag)", "Tracks", &imag_2_0, max_scale, on_trackbar );
 
-    createTrackbar( "Phi(3,3) (real)", "Tracks", &real_3_3, max_3_3, on_trackbar );
-    createTrackbar( "Phi(3,3) (imag)", "Tracks", &imag_3_3, max_3_3, on_trackbar );
+    createTrackbar( "Phi(3,3) (real)", "Tracks", &real_3_3, max_scale, on_trackbar );
+    createTrackbar( "Phi(3,3) (imag)", "Tracks", &imag_3_3, max_scale, on_trackbar );
 
-    createTrackbar( "Phi(3,1) (real)", "Tracks", &real_3_1, max_3_1, on_trackbar );
-    createTrackbar( "Phi(3,1) (imag)", "Tracks", &imag_3_1, max_3_1, on_trackbar );
+    createTrackbar( "Phi(3,1) (real)", "Tracks", &real_3_1, max_scale, on_trackbar );
+    createTrackbar( "Phi(3,1) (imag)", "Tracks", &imag_3_1, max_scale, on_trackbar );
+
+    createTrackbar( "Phi(4,4) (real)", "Tracks", &real_4_4, max_scale, on_trackbar );
+    createTrackbar( "Phi(4,4) (imag)", "Tracks", &imag_4_4, max_scale, on_trackbar );
+
+    createTrackbar( "Phi(4,2) (real)", "Tracks", &real_4_2, max_scale, on_trackbar );
+    createTrackbar( "Phi(4,2) (imag)", "Tracks", &imag_4_2, max_scale, on_trackbar );
+
+    createTrackbar( "Phi(4,0) (real)", "Tracks", &real_4_0, max_scale, on_trackbar );
+    createTrackbar( "Phi(4,0) (imag)", "Tracks", &imag_4_0, max_scale, on_trackbar );
 
     on_trackbar( 0, 0 );
 
